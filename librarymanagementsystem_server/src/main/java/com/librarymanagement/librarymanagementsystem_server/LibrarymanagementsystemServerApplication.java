045163f649188b282020629164de919748c4caab
ASTRO063 <manikanta.kodimala3@gmail.com>
2021-04-06 00:14:37 +0530

    first hellow world api

---
+++ librarymanagementsystem_server/src/main/java/com/librarymanagement/librarymanagementsystem_server/LibrarymanagementsystemServerApplication.java
@@ -0,0 +1,13 @@
+package com.librarymanagement.librarymanagementsystem_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarymanagementsystemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarymanagementsystemServerApplication.class, args);
+	}
+
+}
---
+++ librarymanagementsystem_server/src/test/java/com/librarymanagement/librarymanagementsystem_server/LibrarymanagementsystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarymanagement.librarymanagementsystem_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymanagementsystemServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
